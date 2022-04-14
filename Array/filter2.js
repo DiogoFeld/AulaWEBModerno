b50490f2c26500fe7d40c0679ae316823699c5ea
DiogoFeld <43175839+DiogoFeld@users.noreply.github.com>
2022-04-14 17:11:57 -0300

    Add files via upload

---
+++ Array/filter2.js
@@ -0,0 +1,23 @@
+
+
+
+const produtos = [
+    {nome: "copo de plastico",  preco: 8.4, fragil: false},
+    {nome: "copo de vidro",  preco: 15.47, fragil: true},
+    {nome: "copo de metal",  preco: 48.7, fragil: false}
+]
+
+
+console.log(produtos.filter(function(obj){
+    return obj.nome.includes("vidro")
+}))
+
+
+const caro = p => p.preco > 20;
+const fragil = p => p.fragil;
+
+console.log("produtos Caros");
+console.log(produtos.filter(caro))
+
+console.log("produtos frageis");
+console.log(produtos.filter(fragil))
